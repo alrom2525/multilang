d785ed4192fe91ef94ace6683fe856626cbfcddd
alrom2525 <edgardoalbertoromero@gmail.com>
2020-01-20 13:24:11 -0500

    Laravel Language Switcher

---
+++ resources/lang/en/common.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Questionnaires
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | 
+    | 
+    |
+    */
+    'laravel' => "Laravel",
+    'forgot_pass' =>"Forgot your password?",
+    'email'=> "E-Mail Address",
+    'password'=> "Password",
+    'confirm_pass'=> "Confirm password",
+    'name' => "Name",
+    'register' => 'Register',
+    'login' => "Login",
+    'remember' => "Remember me",   
+    'instruction'     => 'Give choices that give you the most insight into your question',
+];
---
+++ resources/lang/fr/common.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Questionnaires - FR
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | 
+    | 
+    |
+    */
+    'laravel' => "Laravel vFr",
+    'forgot_pass' =>"Vous avez oublié votre mot de passe ?",
+    'email'=> "Address courriel", 
+    'password'=> "Mot de passe",
+    'confirm_pass'=> "Confirmer votre mot de passe",
+    'name' => "Nom",
+    'register' => "S'inscrire",
+    'login' => "Se connecter",
+    'remember' => "Se souvenir du nom d'utilisateur",
+    'instruction'     => 'Donnez des choix qui vous donnent le plus dinformations sur votre question.',
+];
